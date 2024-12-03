6c4a5268d8bbb95cd80d371ff654a433dfcee713
Vetle Lindbråten <vetlelind@gmail.com>
2024-12-03 13:02:25 +0100

    set up template

---
+++ src/types/example.d.ts
@@ -0,0 +1,11 @@
+declare global {    
+    // TODO: Rename Example to your namespace, as well as file name
+    namespace Example {
+        // Define interfaces here
+        interface ExampleInterface {
+            example: string;
+        }
+    }   
+}
+
+export {};
