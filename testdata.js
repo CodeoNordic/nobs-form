dbcb9a0c8dff236ca75c414f083d8f51ddda85f5
Vetle Lindbråten <vetlelind@gmail.com>
2024-12-05 11:23:11 +0100

    updated testdata, fixed styling and updated test fmp file with more scripts and buttons

--- testdata.js
+++ testdata.js
@@ -1,5 +1,5 @@
 init(JSON.stringify({
-    value: '{"title":"test","description":"test test","logoPosition":"right","pages":[{"name":"page1","elements":[{"type":"boolean","name":"question1","title":"test"},{"type":"text","name":"question2","visibleIf":"{question1} = false","title":"Vises pga svart nei"},{"type":"text","name":"question3","visibleIf":"{question1} = true","title":"Vises pga svart ja"}]}]}',
-    type: 'editor',
+    value: '{"title":"test","description":"test test","logoPosition":"right","pages":[{"name":"side1","elements":[{"type":"boolean","name":"spørsmål1","title":"test"},{"type":"text","name":"spørsmål2","visibleIf":"{spørsmål1} = false","title":"Vises pga svart nei"},{"type":"text","name":"spørsmål3","visibleIf":"{spørsmål1} = true","title":"Vises pga svart ja"}]}]}',
+    type: 'builder',
     locale: 'no',
 }))
