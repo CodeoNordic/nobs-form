36cbf397af27eb2045cacf2d4eba40c4d3bd1aca
Vetle Lindbråten <vetlelind@gmail.com>
2025-05-21 16:07:29 +0200

    started fixing localizations for norwegian

--- src/components/Form/Builder.tsx
+++ src/components/Form/Builder.tsx
@@ -1,10 +1,12 @@
 import { editorLocalization, SurveyCreator, SurveyCreatorComponent } from "survey-creator-react";
 import { surveyLocalization } from "survey-react-ui";
 import { useConfig } from "@context/Config";
-import "survey-creator-core/i18n";
+import "survey-creator-core/i18n/norwegian";
+import "survey-core/i18n/norwegian";
 import { useMemo } from "react";
-import { Serializer } from "survey-core";
+import { getLocaleString, Serializer } from "survey-core";
 import performScript from "@utils/performScript";
+import { getLocString } from "survey-creator-core";
 
 const FormBuilder: FC = () => {
     const config = useConfig();
@@ -43,19 +45,74 @@ const FormBuilder: FC = () => {
             )
         };
 
-        // Because it's "nb" instead of "no"
+        // // Because it's "nb" instead of "no"
         const locale = config.locale === "en" ? "en" : "nb";
         editorLocalization.defaultLocale = locale;
         editorLocalization.currentLocale = locale;
     
-        // Normal here
+        // // Normal here
         surveyLocalization.defaultLocale = config.locale;
         surveyLocalization.currentLocale = config.locale;
         surveyLocalization.supportedLocales = ["no", "en"];
     
+        console.log(
+            surveyLocalization.locales,
+            editorLocalization.locales
+        )
+
+        if (config.locale === "no") {
+            // Some custom localizations for Norwegian
+            (editorLocalization.locales as any)["nb"].qt.boolean = "Ja/Nei";
+            (editorLocalization.locales as any)["nb"].pe.labelTrue = "\"Ja\" etikett";
+            (editorLocalization.locales as any)["nb"].pe.labelFalse = "\"Nei\" etikett";
+            (editorLocalization.locales as any)["nb"].pv.inputType.number = "Tall";
+            (editorLocalization.locales as any)["nb"].pv.hidden = "Skjult";
+            (editorLocalization.locales as any)["nb"].pv.hide = "Skjul";
+            (editorLocalization.locales as any)["nb"].pv.show = "Vis";
+            (editorLocalization.locales as any)["nb"].pv.none = "Ingen";
+            (editorLocalization.locales as any)["nb"].pv.asc = "Stigende";
+            (editorLocalization.locales as any)["nb"].pv.desc= "Synkende";
+            (editorLocalization.locales as any)["nb"].pv.default = "Standard";
+            (editorLocalization.locales as any)["nb"].pv.top = "Topp";
+            (editorLocalization.locales as any)["nb"].pv.bottom = "Bunn";
+            (editorLocalization.locales as any)["nb"].pv.left = "Venstre";
+            (editorLocalization.locales as any)["nb"].pv.collapsed = "Kollapset";
+            (editorLocalization.locales as any)["nb"].pv.expanded = "Utvidet";
+            (editorLocalization.locales as any)["nb"].pv.edit = "Redigering";
+            (editorLocalization.locales as any)["nb"].pv.page = "Side";
+            (editorLocalization.locales as any)["nb"].pv.survey = "Undersøkelse";
+            (editorLocalization.locales as any)["nb"].pv.display = "Kun lesbart";
+            (editorLocalization.locales as any)["nb"].pv.inherit = "Arv";
+            (editorLocalization.locales as any)["nb"].pv.onNextPage = "Ved neste side";
+            (editorLocalization.locales as any)["nb"].pv.onValueChanged = "Ved endring av verdi";
+            (editorLocalization.locales as any)["nb"].pv.random = "Tilfeldig";
+            (editorLocalization.locales as any)["nb"].pv.noPreview = "Ingen forhåndsvisning";
+            (editorLocalization.locales as any)["nb"].pv.onComplete = "Ved fullføring";
+            (editorLocalization.locales as any)["nb"].pv.onHidden = "Ved skjuling";
+            (editorLocalization.locales as any)["nb"].pv.showAllQuestions = "Vis alle spørsmål";
+            (editorLocalization.locales as any)["nb"].pv.showAnsweredQuestions = "Vis besvarte spørsmål";
+            (editorLocalization.locales as any)["nb"].pv.underRowSingle = "Under raden, bare ett panel er synlig";
+            (editorLocalization.locales as any)["nb"].p.navigationButtonsVisibility = "Navigasjonsknapper synlighet";
+            (editorLocalization.locales as any)["nb"].p.validators = "Valideringsalternativer";
+            (editorLocalization.locales as any)["nb"].p.choices = "Alternativer";
+            (editorLocalization.locales as any)["nb"].triggers.completetrigger = "fullføre undersøkelse";
+            (editorLocalization.locales as any)["nb"].triggers.setvaluetrigger = "sett verdi";
+            (editorLocalization.locales as any)["nb"].triggers.copyvaluetrigger = "kopier verdi";
+
+            (editorLocalization.locales as any)["nb"].pv.backgroundImageFit = {
+                auto: "Auto",
+                cover: "Dekk", 
+                contain: "Tilpass",
+                fill: "Fyll",
+                tile: "Flislegg" 
+            }
+            (editorLocalization.locales as any)["nb"].pehelp.logofit = "TODO: fix"
+                    
+        }
+
         const newCreator = new SurveyCreator(creatorOptions);
 
-        newCreator.survey.onAfterRenderSurvey.add((s) => { s.navigationBar.getActionById("sv-nav-complete").visible = false; });
+        newCreator.survey.locale = config.locale;
 
         if (config.questionTypes !== undefined) {
             if (Array.isArray(config.questionTypes)) {
